ecf5e8fa03b6ea43f08efc4c8495de30972d8cc1
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-04-18 21:03:14 +0900

    Move the code for lfToCrlf to lftocrlf.go

--- dump.go
+++ dump.go
@@ -8,8 +8,6 @@ import (
 	"fmt"
 	"io"
 	"unicode/utf8"
-
-	"golang.org/x/text/transform"
 )
 
 var (
@@ -17,33 +15,6 @@ var (
 	flagNullString     = flag.String("null", "<NULL>", "Set a string representing NULL")
 	flagTsv            = flag.Bool("tsv", false, "Use TAB as seperator")
 )
-
-type lfToCrlf struct{}
-
-func (t lfToCrlf) Reset() {}
-
-func (f lfToCrlf) Transform(dst, src []byte, atEOF bool) (nDst, nSrc int, err error) {
-	for _, c := range src {
-		if c == '\n' {
-			if len(dst) < 2 {
-				return nDst, nSrc, transform.ErrShortDst
-			}
-			dst[0] = '\r'
-			dst[1] = '\n'
-			dst = dst[2:]
-			nDst += 2
-		} else {
-			if len(dst) < 1 {
-				return nDst, nSrc, transform.ErrShortDst
-			}
-			dst[0] = c
-			dst = dst[1:]
-			nDst++
-		}
-		nSrc++
-	}
-	return nDst, nSrc, nil
-}
 
 func dumpRows(ctx context.Context, rows *sql.Rows, w io.Writer) error {
 	columns, err := rows.Columns()

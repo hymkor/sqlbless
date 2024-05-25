8b6285306923890de5fa36d7787000feb17f28e1
HAYAMA_Kaoru <iyahaya@nifty.com>
2024-05-25 09:51:25 +0900

    Add test-sqlserver.ps1

---
+++ test-sqlserver.ps1
@@ -0,0 +1,70 @@
+$testLst = "output.lst"
+
+if ( (Test-Path $testLst) ){
+    Remove-Item $testLst
+}
+
+$script = `
+    "DROP TABLE TESTTBL||" +
+    "CREATE TABLE TESTTBL|" +
+    "( TESTNO  NUMERIC ,|" +
+    "  DTTM    DATETIME ,|" +
+    "  DT      DATE ,|" +
+    "  TM      TIME ,|" +
+    " PRIMARY  KEY (TESTNO) ); ||" +
+    "INSERT INTO TESTTBL VALUES|" +
+    "(10,CONVERT(DATETIME,'2024-05-25 13:45:33',120),|" +
+    " CONVERT(DATE,'2024-05-26',23),|"+
+    " CONVERT(TIME,'14:53:26',108)) ||" +
+    "COMMIT||" +
+    "EDIT TESTTBL||" +
+    "/10|lr2015-06-07 20:21:22|lr2016-07-08|lr15:54:27|qyy" +
+    "SPOOL $testLst||" +
+    "SELECT * FROM TESTTBL||" +
+    "SPOOL OFF ||" +
+    "ROLLBACK||" +
+    "DROP TABLE TESTTBL||" +
+    "EXIT ||"
+
+$conn = $null
+Get-Content .\tstdblst | Where-Object { $_ -like "*sqlserver*" } | ForEach-Object {
+    $field = ($_ -split "\|")
+    $conn = $field[1]
+    Write-Host "Found $conn"
+}
+if ( -not $conn ){
+    Write-Host "Connection String not found"
+    exit 1
+}
+
+.\sqlbless.exe -auto "$script" sqlserver "$conn"
+
+$ok = $false
+
+Get-Content $testLst |
+Where-Object { $_ -notlike "#*" } |
+Select-Object -skip 1 |
+ForEach-Object {
+    $field = ($_ -split ",")
+    if ( $field.Length -lt 4 ){
+        return
+    }
+    if ( $field[1] -ne "2015-06-07 20:21:22" ){
+        return
+    }
+    if ( $field[2] -notlike "2016-07-08*" ){
+        return
+    }
+    if ( $field[3] -notlike "*15:54:27" ){
+        return
+    }
+    $ok = $true
+}
+
+if ( $ok ){
+    Write-Host "--> OK"
+    exit 0
+} else {
+    Write-Host "--> NG"
+    exit 1
+}

7fcd5be69ce7e06f171c8ef7a232a125f5782650
HAYAMA_Kaoru <iyahaya@nifty.com>
2024-06-01 23:33:38 +0900

    Implement test for timestamp for oracle

--- test/test-oracle.ps1
+++ test/test-oracle.ps1
@@ -12,12 +12,14 @@ $script = `
     "CREATE TABLE TESTTBL|" +
     "( TESTNO  NUMERIC ,|" +
     "  DT      DATE ,|" +
+    "  ST      TIMESTAMP,|" +
     " PRIMARY  KEY (TESTNO) ); ||" +
     "INSERT INTO TESTTBL VALUES|",
-    "(10,TO_DATE('2024-05-25 13:45:33','YYYY-MM-DD HH24:MI:SS')) ||" +
+    "(10,TO_DATE('2024-05-25 13:45:33','YYYY-MM-DD HH24:MI:SS'),|" +
+    " TO_TIMESTAMP('2024-07-08 17:18:19.8787','YYYY-MM-DD HH24:MI:SS.FF')) ||" +
     "COMMIT||" +
     "EDIT TESTTBL||" +
-    "/10|lr2015-06-07 20:21:22|cyy" +
+    "/10|lr2015-06-07 20:21:22|lr2024-08-09 10:11:12.7878|cyy" +
     "SPOOL $testLst||" +
     "SELECT * FROM TESTTBL||" +
     "SPOOL OFF ||" +
@@ -45,8 +47,10 @@ Where-Object { $_ -notlike "#*" } |
 Select-Object -skip 1 |
 ForEach-Object {
     $field = ($_ -split ",")
-    if ( $field.Length -ge 2 -and $field[1] -eq "2015-06-07 20:21:22" ){
-        Write-Host ("Found {0} --> OK" -f $field[1])
+    if ( $field.Length -ge 3 -and 
+        $field[1] -eq "2015-06-07 20:21:22" -and
+        $field[2] -eq "2024-08-09 10:11:12.7878" ){
+        Write-Host ("Found {0} and {1} --> OK" -f ($field[1],$field[2]))
         exit 0
     }
 }

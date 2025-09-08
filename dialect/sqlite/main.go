530a21bf2ef4e9de5bdf692ec48be54e2cf1c733
HAYAMA Kaoru <3752189+hymkor@users.noreply.github.com>
2025-09-09 02:50:29 +0900

    Fix: failed to compare DATE column on SQLite3

--- dialect/sqlite/main.go
+++ dialect/sqlite/main.go
@@ -1,6 +1,8 @@
 package sqlite
 
 import (
+	"fmt"
+
 	_ "github.com/glebarez/go-sqlite/compat"
 
 	"github.com/hymkor/sqlbless/dialect"
@@ -15,9 +17,29 @@ var Entry = &dialect.Entry{
 	union all
 	select 'TEMP_SCHEMA' AS SCHEMA,* FROM sqlite_temp_schema`,
 	DisplayDateTimeLayout: dateTimeTzLayout,
+	TypeNameToConv:        typeNameToConv,
 	SqlForDesc:            `PRAGMA table_info({table_name})`,
 	TableField:            "tbl_name",
 	ColumnField:           "name",
+}
+
+var typeNameToFormat = map[string]string{
+	"TIMESTAMP": "2006-01-02 15:04:05.999999999-07:00",
+	"TIME":      dialect.TimeOnlyLayout,
+	"DATE":      dialect.DateOnlyLayout,
+}
+
+func typeNameToConv(typeName string) func(string) (string, error) {
+	if format, ok := typeNameToFormat[typeName]; ok {
+		return func(s string) (string, error) {
+			dt, err := dialect.ParseAnyDateTime(s)
+			if err != nil {
+				return "", err
+			}
+			return fmt.Sprintf("'%s'", dt.Format(format)), nil
+		}
+	}
+	return nil
 }
 
 func init() {

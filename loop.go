eafc4d33a26cdec0324c9aaf6048ef3d45bd7db6
HAYAMA Kaoru <3752189+hymkor@users.noreply.github.com>
2025-10-12 02:40:18 +0900

    Move the code for `Script` from `loop.go` to `script.go`

--- loop.go
+++ loop.go
@@ -1,7 +1,6 @@
 package sqlbless
 
 import (
-	"bufio"
 	"context"
 	"database/sql"
 	"encoding/csv"
@@ -179,51 +178,6 @@ type CommandIn interface {
 	AutoPilotForCsvi() (getKeyAndSize, bool)
 }
 
-type Script struct {
-	br   *bufio.Reader
-	echo io.Writer
-	term string
-}
-
-func (script *Script) GetKey() (string, error) {
-	return "", io.EOF
-}
-
-func (script *Script) AutoPilotForCsvi() (getKeyAndSize, bool) {
-	return nil, false
-}
-
-func (script *Script) Read(context.Context) ([]string, error) {
-	var buffer strings.Builder
-	quoted := 0
-	for {
-		ch, _, err := script.br.ReadRune()
-		if err != nil {
-			code := buffer.String()
-			fmt.Fprintln(script.echo, code)
-			return []string{code}, err
-		}
-		if ch == '\r' {
-			continue
-		} else if ch == '\'' {
-			quoted ^= 1
-		} else if ch == '"' {
-			quoted ^= 2
-		}
-		buffer.WriteRune(ch)
-
-		if quoted == 0 {
-			code := buffer.String()
-			term := script.term
-			if _, ok := hasTerm(code, term); ok {
-				println(code)
-				fmt.Fprintln(script.echo, code)
-				return []string{code}, nil
-			}
-		}
-	}
-}
-
 type Session struct {
 	*Config
 	Dialect         *dialect.Entry
@@ -249,32 +203,6 @@ func (ss *Session) Close() {
 
 func (ss *Session) automatic() bool {
 	return ss.Auto != ""
-}
-
-func (ss *Session) StartFromStdin(ctx context.Context) error {
-	script := &Script{
-		br:   bufio.NewReader(os.Stdin),
-		echo: ss.stdErr,
-		term: ss.Term,
-	}
-	return ss.Loop(ctx, script, true)
-}
-
-func (ss *Session) Start(ctx context.Context, fname string) error {
-	if fname == "-" {
-		return ss.StartFromStdin(ctx)
-	}
-	fd, err := os.Open(fname)
-	if err != nil {
-		return err
-	}
-	defer fd.Close()
-	script := &Script{
-		br:   bufio.NewReader(fd),
-		echo: ss.stdErr,
-		term: ss.Term,
-	}
-	return ss.Loop(ctx, script, true)
 }
 
 func (ss *Session) Loop(ctx context.Context, commandIn CommandIn, onErrorAbort bool) error {
---
+++ script.go
@@ -0,0 +1,81 @@
+package sqlbless
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+type Script struct {
+	br   *bufio.Reader
+	echo io.Writer
+	term string
+}
+
+func (script *Script) GetKey() (string, error) {
+	return "", io.EOF
+}
+
+func (script *Script) AutoPilotForCsvi() (getKeyAndSize, bool) {
+	return nil, false
+}
+
+func (script *Script) Read(context.Context) ([]string, error) {
+	var buffer strings.Builder
+	quoted := 0
+	for {
+		ch, _, err := script.br.ReadRune()
+		if err != nil {
+			code := buffer.String()
+			fmt.Fprintln(script.echo, code)
+			return []string{code}, err
+		}
+		if ch == '\r' {
+			continue
+		} else if ch == '\'' {
+			quoted ^= 1
+		} else if ch == '"' {
+			quoted ^= 2
+		}
+		buffer.WriteRune(ch)
+
+		if quoted == 0 {
+			code := buffer.String()
+			term := script.term
+			if _, ok := hasTerm(code, term); ok {
+				println(code)
+				fmt.Fprintln(script.echo, code)
+				return []string{code}, nil
+			}
+		}
+	}
+}
+
+func (ss *Session) StartFromStdin(ctx context.Context) error {
+	script := &Script{
+		br:   bufio.NewReader(os.Stdin),
+		echo: ss.stdErr,
+		term: ss.Term,
+	}
+	return ss.Loop(ctx, script, true)
+}
+
+func (ss *Session) Start(ctx context.Context, fname string) error {
+	if fname == "-" {
+		return ss.StartFromStdin(ctx)
+	}
+	fd, err := os.Open(fname)
+	if err != nil {
+		return err
+	}
+	defer fd.Close()
+	script := &Script{
+		br:   bufio.NewReader(fd),
+		echo: ss.stdErr,
+		term: ss.Term,
+	}
+	return ss.Loop(ctx, script, true)
+}

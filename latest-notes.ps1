27dd64bb5629e031d37ecc85e55ec8900cd88f9e
HAYAMA Kaoru <3752189+hymkor@users.noreply.github.com>
2025-11-03 17:12:58 +0900

    Update latest_notes.ps1

--- latest-notes.ps1
+++ latest-notes.ps1
@@ -1,12 +1,13 @@
 Set-PSDebug -Strict
 
 $saveEncode = $null
-if ([Console]::IsOutputRedirected) {
+if ([Console]::IsOutputRedirected ) {
     $saveEncode = [System.Console]::OutputEncoding
     [System.Console]::OutputEncoding=[System.Text.Encoding]::UTF8
 }
 
-Get-ChildItem "release_note*" | Sort-Object { Format-Hex -InputObject $_.Name } | ForEach-Object{
+$blanklines = $null
+Get-ChildItem "release_note*.md" -Recurse | Sort-Object { Format-Hex -InputObject $_.FullName } | ForEach-Object{
     $lang = "(English)"
     if ( $_.FullName -like "*ja*" ) {
         $lang = "(Japanese)"
@@ -14,19 +15,32 @@ Get-ChildItem "release_note*" | Sort-Object { Format-Hex -InputObject $_.Name } 
     $flag = 0
     $section = 0
     Get-Content $_.FullName | ForEach-Object {
-        if ( $_ -match "^v[0-9]+\.[0-9]+\.[0-9]+$" ){
+        if ( $_ -match "^v?[0-9]+\.[0-9]+\.[0-9]+" ){
             $flag++
             if ( $flag -eq 1 ){
-                Write-Host ("`r`n### Changes in {0} in {1}" -f ($_,$lang))
+                Write-Output ""
+                Write-Output ("### Changes in {0} in {1}" -f ($_,$lang))
             }
         } elseif ($flag -eq 1 ){
             if ( $_ -eq "" ){
                 $section++
             }
-            if ( $section % 2 -eq 1 ){
-                Write-Host $_
+            if ( $section -ge 1 ){
+                Write-Output $_
             }
         }
+    }
+} | ForEach-Object {
+    if ( $_ -match "^\s*$" ){
+        if ( $blanklines -ne $null ){
+            $blanklines = $true
+        }
+    } else {
+        if ( $blanklines ){
+            Write-Output ""
+        }
+        Write-Output $_
+        $blanklines = $false
     }
 }
 

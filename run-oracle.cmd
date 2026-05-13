4e5960d35ec7cb2a65349e22f52f3804ea47688b
HAYAMA Kaoru <3752189+hymkor@users.noreply.github.com>
2026-05-13 21:21:19 +0900

    Unify Windows launch scripts as run.cmd

--- run-oracle.cmd
+++
@@ -1,6 +0,0 @@
-@rem Sample script to launch SQL-Bless with Oracle DB
-
-@setlocal
-@set PROMPT=$G$S
-@rem $ sqlbless oracle://USERNAME:PASSWORD@HOST:PORT/SERVICE
-sqlbless %* oracle://scott:tiger@localhost:1521/xepdb1
